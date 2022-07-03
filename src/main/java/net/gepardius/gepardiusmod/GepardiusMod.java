acaf1c6d8e64267717a2682d48f2497a98bc9a44
Aleksandar <aleksandar.bordelius@gmail.com>
2022-07-03 20:56:13 +0200

    item: gepardie added

--- src/main/java/net/gepardius/gepardiusmod/GepardiusMod.java
+++ src/main/java/net/gepardius/gepardiusmod/GepardiusMod.java
@@ -1,9 +1,11 @@
 package net.gepardius.gepardiusmod;
 
+import net.gepardius.gepardiusmod.item.ModItems;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
@@ -26,7 +28,12 @@ public class GepardiusMod {
 
     public GepardiusMod() {
         // Register the setup method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+
+        // registering mod items into our register
+        ModItems.register(eventBus);
+        eventBus.addListener(this::setup);
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
---
+++ src/main/java/net/gepardius/gepardiusmod/item/ModItems.java
@@ -0,0 +1,24 @@
+package net.gepardius.gepardiusmod.item;
+
+import net.gepardius.gepardiusmod.GepardiusMod;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, GepardiusMod.MOD_ID);
+
+    public static final RegistryObject<Item> GEPARDIE = ITEMS.register("gepardie",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+
+}
